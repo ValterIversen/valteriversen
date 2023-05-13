e25c7b655c3303744d0a443c7c4beecd90a3c388
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-05-13 15:39:56 -0300

    Criação do projeto

---
+++ components/Header.js
@@ -0,0 +1,75 @@
+'use client'
+import React, { useState } from 'react';
+
+const Header = ({children}) => {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <div className="overflow-x-hidden bg-gray-50">
+        <header className="py-4 md:py-6" x-data="{expanded: false}">
+            <div className="container px-4 mx-auto sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between">
+                    <div className="flex-shrink-0">
+                        <a href="#" title="" className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2">
+                            <img className="w-auto h-8" src="https://cdn.rareblocks.xyz/collection/clarity/images/logo.svg" alt="" />
+                        </a>
+                    </div>
+        
+                    <div className="flex lg:hidden">
+                        <button type="button" className="text-gray-900" onClick={() => setExpanded(!expanded)} aria-expanded={expanded}>
+                            {
+                                !expanded &&
+                                (
+                                <span x-show="!expanded" aria-hidden="true">
+                                    <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4 6h16M4 12h16M4 18h16" />
+                                    </svg>
+                                </span>
+                                )
+                            }
+                            {
+                                expanded &&
+                                <span x-show="expanded" aria-hidden="true">
+                                    <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </span>
+                                
+                            }
+                        </button>
+                    </div>
+        
+                    <div className="hidden lg:flex lg:ml-16 lg:items-center lg:justify-center lg:space-x-10 xl:space-x-16">
+                        <a href="#" title="" className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Portfolio </a>
+        
+                        <a href="#" title="" className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Quem sou eu </a>
+        
+                        <a href="#" title="" className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Entre em contato </a>
+                    </div>
+        
+                    <div className="hidden lg:ml-auto lg:flex lg:items-center lg:space-x-10">
+                    </div>
+                </div>
+        
+                {
+                    expanded &&
+                    <nav x-show="expanded">
+                        <div className="px-1 py-8">
+                            <div className="grid gap-y-7">
+                                <a href="#" title="" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Portfolio </a>
+            
+                                <a href="#" title="" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Quem sou eu </a>
+            
+                                <a href="#" title="" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Entre em contato </a>
+                            </div>
+                        </div>
+                    </nav>
+                }
+            </div>
+        </header>
+        {children}
+    </div>
+    )
+}
+
+export default Header;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import ProjectsHero from '../../components/ProjectsHero'
+import Header from '../../components/Header'
+
+export default function Home() {
+  return (
+    <Header>
+      <ProjectsHero />
+    </Header>
+  )
+}
