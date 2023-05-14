332a62b39d5d0a4c29e8667c05c4fcf30689a668
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-05-14 02:12:33 -0300

    Definição máxima about

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+    return(
+        <span>
+            Qualidade é o alicerce que sustenta a grandeza de um software - como desenvolvedor e analista de qualidade, busco construir soluções que resistam ao tempo e transcendam as expectativas.
+        </span>
+    )
+}
+
+export default About;
