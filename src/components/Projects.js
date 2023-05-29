339553b21f786a35f57cc69972f8a4dabac7e5f3
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-05-29 20:40:01 -0300

    Melhoria gráfica play/stop

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -60,27 +60,23 @@ const Project = ({projectData}) => {
                      ref={videoRef} loop muted>      
                     <source src={video} type="video/mp4"/>       
                 </video>
-                {
-                    !playing &&
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="black w-14 h-14
-                    text-black bg-white bg-clip-padding bg-opacity-60 backdrop-blur-xl top-0 rounded-full p-0
-                    absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                    </svg>
-                }
-
-                {
-                    !playing &&
-                        <div className="text-black text-sm lg:text-base
-                        absolute bottom-0 bg-white shadow-lg bg-clip-padding bg-opacity-60 border border-gray-200 backdrop-blur-xl
-                        border-none px-10 transition-height duration-1000 ease-in-out w-full h-24 lg:h40 p-0">
-                            <p className="font-bold text-center mt-2 lg:mt-7">{title}</p>
-                            <p className="text-center mt-0 lg:mt-3">{subtitle}</p>
-                            { github && <p className="text-center mt-3 font-medium"><Link href={github}>Acesse o projeto</Link></p>}
-                            { link && <p className="text-center mt-3 font-medium"><Link href={link}>Acesse o site</Link></p>}
-                        </div>
-                }
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" 
+                className={`black w-14 h-14 transition-opacity duration-1000 ease-in-out
+                text-black bg-white bg-clip-padding bg-opacity-60 backdrop-blur-xl top-0 rounded-full p-0
+                absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+                ${playing ? "opacity-0" : "opacity-1"}`}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+                </svg>
+                <div className={`text-black text-sm lg:text-base
+                absolute bottom-0 bg-white shadow-lg bg-clip-padding bg-opacity-60 border border-gray-200 backdrop-blur-xl
+                border-none px-10 transition-height duration-1000 ease-in-out w-full p-0
+                ${playing ? "h-0 lg:h-0" : "h-16 lg:h-40"}`}>
+                    <p className="font-bold text-center mt-2 lg:mt-7">{title}</p>
+                    <p className="text-center mt-0 lg:mt-3 hidden lg:block">{subtitle}</p>
+                    { github && <p className="text-center mt-2 lg:mt-3 font-medium text-sky-800 underline"><Link href={github}>Acesse o projeto</Link></p>}
+                    { link && <p className="text-center mt-2 lg:mt-3 font-medium text-sky-800 underline"><Link href={link}>Acesse o site</Link></p>}
+                </div>
             </div>
         </div>
     )
