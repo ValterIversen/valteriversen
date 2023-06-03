32b02c25c163ca7a1d71f60e7a79cedb58b8f171
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-06-02 23:56:56 -0300

    Sobre

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
-
+import Image from 'next/image';
 
 const About = () => {
     return(
-        <div id="about">
-            <span>
-                Qualidade é o alicerce que sustenta a grandeza de um software - como desenvolvedor e analista de qualidade, busco construir soluções que resistam ao tempo e transcendam as expectativas.
-            </span>
+        <div id="about" className="grid grid-cols-2 text-black h-screen">
+            <div className="my-10 mx-32">
+                <span className="max-w-40">
+                    Qualidade é o alicerce que sustenta a grandeza de um software - como desenvolvedor e analista de qualidade, busco construir soluções que resistam ao tempo e transcendam as expectativas.
+                </span>
+            </div>
+            <div className="">
+
+            </div>
         </div>
     )
 }
