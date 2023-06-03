34bb4bbc2efbe9fbd76027159599b2c6c2f98ed2
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-06-03 00:58:03 -0300

    Ajuste about e envio imagem

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,20 @@ import Image from 'next/image';
 
 const About = () => {
     return(
-        <div id="about" className="grid grid-cols-2 text-black h-screen">
-            <div className="my-10 mx-32">
+        <div id="about" className="relative h-screen">
+            <div className="text-black">
                 <span className="max-w-40">
                     Qualidade é o alicerce que sustenta a grandeza de um software - como desenvolvedor e analista de qualidade, busco construir soluções que resistam ao tempo e transcendam as expectativas.
                 </span>
             </div>
-            <div className="">
+            <div className="absolute bottom-0 right-3">
+                <Image
+                className="mb-0 mt-auto"
+                    src="/perfil.png"
+                    width={700}
+                    height={700}
+                    alt="Picture of the author"
+                    />
 
             </div>
         </div>
