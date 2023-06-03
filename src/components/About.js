c82500bcb88fbf693354c2ebda17b4f17dd97028
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2023-06-03 18:55:44 -0300

    Revert "Mudança fonte About"
    This reverts commit 7fb82e288ff12822647708f8700b32d023eeb6f7.

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <div className="z-0 px-8 pt-10 
             lg:absolute lg:max-w-2xl lg:left-28 lg:top-1/2 lg:transform lg:-translate-y-1/2
             2xl:max-w-4xl 2xl:left-42">
-                <span className="text-black text-2xl font-bold lg:text-5xl
+                <span className="text-black text-2xl font-medium lg:text-5xl
                 2xl:text-6xl">
                     <span className="relative inline-flex pr-1 mr-1">
                         <span className="bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-lg filter opacity-30 w-full h-full absolute inset-0"></span>
@@ -19,7 +19,7 @@ const About = () => {
                         <span className="relative ml-1 md:ml-0"> grandeza </span>
                     </span> de um software.
                 </span>
-                <p className="text-black font-medium
+                <p className="text-black
                 text-lg mx-5 mt-5
                 lg:text-2xl lg:mx-12 lg:mt-8
                 2xl:text-3xl 2lx:mx-18 2xl:mt-10">
