391f3a8b2d58168449074a4ecb7a95e481c0032d
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2024-11-13 16:52:43 -0300

    Reparo do uso antecipado

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,12 +4,15 @@ import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
  
 export default async function LocaleLayout({
-  children,
-  params: {locale}
-}: {
-  children: React.ReactNode;
-  params: {locale: string};
+    children,
+    params
+  }: {
+    children: React.ReactNode;
+    params: {locale: string};
 }) {
+  
+  const { locale } = await params;
+
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound();
