f571d6f7050323e6e4dcb60424b5cafe6ccdf285
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2024-11-14 15:51:59 -0300

    teste 2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,20 +3,13 @@ import {getMessages} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
  
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({locale}));
-}
- 
 export default async function LocaleLayout({
-    children,
-    params
-  }: {
-    children: React.ReactNode;
-    params: {locale: string};
+  children,
+  params: {locale}
+}: {
+  children: React.ReactNode;
+  params: {locale: string};
 }) {
-  
-  const { locale } = await params;
-
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound();
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,12 @@
 import {useTranslations} from 'next-intl';
-import {Link} from '@/i18n/routing';
 import '../globals.css'  // ou o nome do seu arquivo CSS
 
 import Hero from '../../components/Hero'
 import Header from '../../components/Header'
 import About from '../../components/About'
 import Projects from '../../components/Projects';
-
  
 export default function HomePage() {
-  const t = useTranslations();
   return (
     <div>
     <Header>
