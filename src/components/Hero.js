449399c785e4a34a3792a78b1ad5dd496b5f189f
Valter Iversen Fernandes de Souza <iversen.souza@gmail.com>
2025-07-06 00:58:22 -0300

    Ajuste linkedin
    Safado trocou link

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -39,7 +39,7 @@ const Hero = () => {
                         </a>
 
                         <a
-                            href="https://www.linkedin.com/in/valter-souza-769184198/"
+                            href="https://www.linkedin.com/in/valter-iversen-769184198/"
                             title=""
                             className="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-900 transition-all duration-200 border-2 border-gray-400 sm:w-auto sm:mt-0 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 focus:bg-gray-900 hover:text-white focus:text-white hover:border-gray-900 focus:border-gray-900"
                             role="button"
